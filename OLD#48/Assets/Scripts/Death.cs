5a53a7e0983755addcd757051e9ce5dec3c50cb8
DESKTOP-9Q3AGBV\User <zahzah01>
2017-04-22 13:54:44 +0600

    1

---
+++ OLD#48/Assets/Scripts/Death.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class Death : MonoBehaviour {
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.name == "Player") 
+		{
+			Debug.Log ("1");
+			EditorSceneManager.LoadScene ("2");
+		}
+}
+	void OnParticleEnter (Collider col)
+	{
+		
+			Debug.Log ("1");
+			EditorSceneManager.LoadScene ("2");
+		}
+	}
